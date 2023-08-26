d9ed0150712f865a0f1557b6f7122735fea68873
Administrator <zhangyuedong2017@tom.com>
2023-08-26 04:23:25 +0000

    Update src/org/devops/Build.groovy

--- src/org/devops/Build.groovy
+++ src/org/devops/Build.groovy
@@ -9,7 +9,7 @@ def Build(){
             sh "yarn build"
             break
         case "maven":
-            sh "mvn clean package -DskipTests"
+            sh "/data/devops/tools/apache-maven-3.9.3/bin/mvn clean package -DskipTests"
             break
         case "gradle":
             sh "gradle clean build -x test"
@@ -30,7 +30,7 @@ def UnitTest(){
     if ("${JOB_NAME}".endsWith("-service")) {
         switch("${env.buildTools}") {
             case "maven":
-                sh "mvn test"
+                sh "/data/devops/tools/apache-maven-3.9.3/bin/mvn test"
                 break
             case "gradle":
                 sh "gradle test"
